5cdcfd73c577c3d994d46c80c50d331a5f29f33a
melissa bafdel <melissabafdel@gmail.com>
2020-04-25 01:33:46 +0200

    ajout

--- src/main/java/pglp_5/exo5_1/DAOJBCD.java
+++ src/main/java/pglp_5/exo5_1/DAOJBCD.java
@@ -1,5 +1,39 @@
 package pglp_5.exo5_1;
 
-public class DAOJBCD {
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public  abstract class  DAOJBCD<T> extends DAO<T> {
+	private String BDurl;
+	private Connection conn;
+
+	
+	public DAOJBCD() throws SQLException{
+		BDurl ="jdbc:derby:DATA\\ jdbcDB;create=true";
+		try{
+			SetConnect(DriverManager.getConnection(BDurl));
+		}catch (SQLException e){
+		e.printStackTrace();
+	}
+}
+	
+@Override
+public abstract T create (T obj) throws IOException, SQLException;
+public abstract void delete (T obj) throws SQLException;
+public abstract T update (T obj) throws IOException, SQLException;
+public abstract T find (int id) throws FileNotFoundException, IOException, SQLException;
+	
+
+public Object deserialisation(final byte[] bytes) throws ClassNotFoundException, IOException{
+	 ByteArrayInputStream a = new ByteArrayInputStream(bytes);
+	 ObjectInputStream b = new ObjectInputStream(a);
+	 return b.readObject();
+}
+
 
 }
