3a55105902f81169a7a233fa9fdb458231f34887
melissa bafdel <melissabafdel@gmail.com>
2020-04-28 02:12:24 +0200

    ajout de classes

--- src/main/java/pglp_5/exo5_1/AbstractDAOFactory.java
+++ src/main/java/pglp_5/exo5_1/AbstractDAOFactory.java
@@ -1,9 +1,13 @@
 package pglp_5.exo5_1;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public abstract class AbstractDAOFactory {
 	
 	public enum DAOType{JBDC,FILE}
 	
-	public abstract DAO<NumeroTel> getNuleroTelDAO(); 
-
+	public abstract DAO<NumeroTel> getNuleroTelDAO() throws IOException, SQLException;
+	public abstract DAO <Personnel> getPersonnelDAO()throws IOException, SQLException;
+	public abstract DAO<GroupePersonnel> getGroupePersonnelDAO() throws IOException, SQLException;
 }
