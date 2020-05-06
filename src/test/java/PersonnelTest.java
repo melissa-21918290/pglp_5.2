a786b5d9bad630955fbaa877706dc440fdca2c93
melissa bafdel <melissabafdel@gmail.com>
2020-05-06 18:56:59 +0200

    ajout test

---
+++ src/test/java/PersonnelTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import pglp_5.exo5_1.Personnel.Builder;
+public class PersonnelTest {
+
+}
