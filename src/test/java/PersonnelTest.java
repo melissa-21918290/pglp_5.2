741634b3a5844475621a1eda55f2205c0339b355
melissa bafdel <melissabafdel@gmail.com>
2020-05-06 19:02:55 +0200

    ajout test

--- src/test/java/PersonnelTest.java
+++ src/test/java/PersonnelTest.java
@@ -14,7 +14,50 @@ import java.time.LocalDate;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import pglp_5.exo5_1.DAO;
+import pglp_5.exo5_1.DAOFactoryJDBC;
+import pglp_5.exo5_1.NumeroTel;
+import pglp_5.exo5_1.Personnel;
 import pglp_5.exo5_1.Personnel.Builder;
 public class PersonnelTest {
+	
+	Personnel secretaire;
+	
+	String nomDir;
+	File dir;
+	
+	DAO<Personnel> personnel;
+	
+	@Before
+	public void setUp () throws IOException, SQLException{
+		nomDir = "Personnel";
+		dir = new File(nomDir);
+		personnel = new DAOFactoryJDBC().getPersonnelDAO();
+		NumeroTel portable = new NumeroTel("portable","0698102210", 1);
+		Builder b = new Builder("bafdel","melissa","secretaire", LocalDate.of(1995, 11, 10),1);
+		b.numTelephones(portable);
+		Personnel p =b.build();
+		secretaire = p;
+				
+	}
+	@Test
+	public void constructeurTest(){
+		  String expDesc = "portable";
+	        String expNum = "0698102210";
+	        String expNom = "bafdel";
+	        String expPrenom = "melissa";
+	        String expFonction = "secrétaire";
+	        LocalDate expDate = LocalDate.of(1995, 11, 10);
+	        assertEquals(expDesc,
+	                secretaire.getNumTelephones().get(0).getDescriptif());
+	        assertEquals(expNum, secretaire.getNumTelephones().get(0).getNumero());
+	        assertEquals(expNom, secretaire.getNom());
+	        assertEquals(expPrenom, secretaire.getPrenom());
+	        assertEquals(expFonction, secretaire.getFonction());
+	        assertEquals(expDate, secretaire.getDateNaissance());
+	}
+	
+	
 
 }
